8cabe0523807774d1cda5a9ba3deb6731c85186d
AkuuAlyaaa <alyasefhia24.@gmail.com>
2024-05-11 14:06:30 +0700

    utscitra

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Profile')
+st.subheader('Nama : Alya Sefhia Eka Putri')
+st.subheader('NIM : 312210108')
+st.subheader('Kelas : TI.22.B1')
+st.subheader('Prodi : Teknik Infomatika')
+
+st.image("./img/alya.jpg", caption="Alya", use_column_width="Always", width=200)
+
+
+st.link_button("Instagram", "https://www.instagram.com/alyasefhia_?igsh=MTA3cXN5eWlnaWRicg%3D%3D")
+st.link_button("Github", "https://github.com/AkuuAlyaaa")
